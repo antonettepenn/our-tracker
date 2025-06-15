74f0f98c20a92ad690491b641fb38982f4ae57a6
antonettepenn <77859337+antonettepenn@users.noreply.github.com>
2025-06-15 10:46:34 -0500

    Added Entry model w/ shared categories

---
+++ Models/Entry.cs
@@ -0,0 +1,8 @@
+public class Entry 
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Type { get; set; } // which type of thing tracked (anime/show, date idea, note, etc)
+    public string Description { get; set; }
+    public DateTime CreatedAt {get; set; } 
+}
