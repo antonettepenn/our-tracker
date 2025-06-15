36c09d17502dbd832136ada271b63e044d5fdbf4
antonettepenn <77859337+antonettepenn@users.noreply.github.com>
2025-06-15 14:59:19 -0500

    Expanded Entry model to support more list types

--- Models/Entry.cs
+++ Models/Entry.cs
@@ -4,7 +4,7 @@ namespace OurTracker.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Type { get; set; } // which type of thing tracked (anime/show, date idea, note, etc)
+        public string Type { get; set; } // which type of thing tracked "date-idea", "bucketlist-date", "show", "note"
         public string Description { get; set; }
         public DateTime CreatedAt {get; set; } 
     }
